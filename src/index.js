12b6d03c5ca986e4ae96752349fa2aaae3900335
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-06 10:05:50 +0300

    route

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 import express from 'express';
-import { pageHome } from './pages/home.js';
-import { pageAbout } from './pages/about.js';
-import { pageUser } from './pages/user.js';
 import { page404 } from './pages/404.js';
-import { textPost } from './api/text/textPost.js';
-import { textPut } from './api/text/textPut.js';
-import { updateCountPut } from './api/update-count/updateCountPut.js';
+import { pageRouter } from './router/pageRouter.js';
+import { apiRouter } from './router/apiRouter.js'
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.get('/', pageHome);
-app.get('/about', pageAbout);
-app.get('/users/:userId', pageUser);
 
-app.all('/api', (req, res) => {
-    return res.status(404).send('Toks adresas negalimas');
-});
-
-app.post('/api/text', textPost);
-app.put('/api/text/:index/', textPut); 
-app.put('/api/update-count/:number', updateCountPut); 
+app.use('/', pageRouter);
+app.use('/', apiRouter);
 
 app.get('*error', page404);
 
