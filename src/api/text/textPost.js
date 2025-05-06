6e96feb6768c3124216727cd2f68fb7ba4ebba84
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-06 09:06:04 +0300

    up

---
+++ src/api/text/textPost.js
@@ -0,0 +1,11 @@
+import { textList } from "../../pages/home.js";
+
+export function textPost(req, res) {  
+
+textList.push(req.body.text);
+
+      return res.json({
+        status: 'success',
+        msg: 'Sukurtas naujas tekstas',
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
 import express from 'express';
-import { count, pageHome, textList } from './pages/home.js';
+import { pageHome } from './pages/home.js';
 import { pageAbout } from './pages/about.js';
 import { pageUser } from './pages/user.js';
 import { page404 } from './pages/404.js';
+import { textPost } from './api/text/textPost.js';
+import { textPut } from './api/text/textPut.js';
+import { updateCountPut } from './api/update-count/updateCountPut.js';
 const app = express();
 const port = 3000;
 
-// kodas, kur padeda gauti POST duomenis?????
-
 app.use(express.static('public'));
-
+app.use(express.json());
 app.get('/', pageHome);
 app.get('/about', pageAbout);
 app.get('/users/:userId', pageUser);
 
 app.all('/api', (req, res) => {
-    res.status(404).send('Toks adresas negalimas');
+    return res.status(404).send('Toks adresas negalimas');
 });
 
-app.post('/api/text', (req, res) => {
-    console.log(req.body);
-
-    // textList.push();
-    res.send('Sukurtas naujas tekstas');
-});
-app.put('/api/text/:index/:text', (req, res) => {
-    textList[req.params.index] = req.params.text.replaceAll('-', ' ');
-    res.send('Tekstas sekmingai atnaujintas');
-});
-
-app.put('/api/update-count/:number', (req, res) => {
-    const n = +req.params.number;
-    if (!isNaN(n)) {
-        count.value = n;
-    }
-    res.send('Gautas skaicius: ' + n);
-});
+app.post('/api/text', textPost);
+app.put('/api/text/:index/', textPut); 
+app.put('/api/update-count/:number', updateCountPut); 
 
 app.get('*error', page404);
 
