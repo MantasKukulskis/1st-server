18da434d9ee0c01a019a40a9b156015e7acba5a4
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-06 16:55:23 +0300

    fech

--- src/api/text/textPost.js
+++ src/api/text/textPost.js
@@ -1,11 +1,17 @@
 import { textList } from "../../pages/home.js";
 
-export function textPost(req, res) {  
+export function textPost(req, res) {
+    if (typeof req.body === 'object' && typeof req.body.text === 'string') {
+        textList.push(req.body.text);
 
-textList.push(req.body.text);
+        return res.json({
+            status: 'success',
+            msg: 'Sukurtas naujas tekstas',
+        });
+    }
 
-      return res.json({
-        status: 'success',
-        msg: 'Sukurtas naujas tekstas',
+    return res.json({
+        status: 'error',
+        msg: 'Teksto sukurti nepavyko',
     });
 }
--- src/router/pageRouter.js
+++ src/router/pageRouter.js
@@ -2,8 +2,10 @@ import express from 'express';
 import { pageHome } from '../pages/home.js';
 import { pageAbout } from '../pages/about.js';
 import { pageUser } from '../pages/user.js';
+import { pageAddText } from '../pages/add.Text.js';
 export const pageRouter = express.Router();
 
 pageRouter.get('/', pageHome);
 pageRouter.get('/about', pageAbout);
 pageRouter.get('/users/:userId', pageUser);
+pageRouter.get('/add-text', pageAddText);
